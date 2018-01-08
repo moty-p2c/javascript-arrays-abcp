b69f67f05301db4c7497f9f9cc1ed4e7e5767077
moty pitusy <moty.p2c@gmail.com>
2018-01-08 11:55:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-let chocolateBars = ["snickers" ,"hundred grand" ,"kitkat" ,"skittles"]
+var chocolateBars = ["snickers" ,"hundred grand" ,"kitkat" ,"skittles"]
 
 function addElementToBeginningOfArray (array ,element){
   let newArray = [element, ...array]
