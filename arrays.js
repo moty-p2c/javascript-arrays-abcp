4a83ee7bc4842de445d897410763460886b2e487
moty pitusy <moty.p2c@gmail.com>
2018-01-08 10:33:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,12 @@
 let chocolateBars = ["snickers" ,"hundred grand" ,"kitkat" ,"skittles"]
 
-function addElementToBeginningOfArray (array ,element ){
+function addElementToBeginningOfArray (array ,element){
+  let newArray = [element, ...array]
   return newArray
 }
 
 
-function destructivelyAddElementToBeginningOfArray(array ,element ){
+function destructivelyAddElementToBeginningOfArray(array ,element){
+  array.unshift(element)
   return array
 }
