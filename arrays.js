d6633e03aa61a954010684eff14e1e262af58113
moty pitusy <moty.p2c@gmail.com>
2018-01-08 10:41:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,4 +25,7 @@ function accessElementInArray(array, index){
   return array[index]
 }
 
-function 
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
