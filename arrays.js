88d42db5fa5fdb6287e254ba9fb42b4d79c2ee84
moty pitusy <moty.p2c@gmail.com>
2018-01-08 10:29:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,10 @@
 let chocolateBars = ["snickers" ,"hundred grand" ,"kitkat" ,"skittles"]
 
-function addElementToBeginningOfArray (){
+function addElementToBeginningOfArray (array ,){
   
 }
 
 
-function destructivelyAddElementToBeginningOfArray(){
+function destructivelyAddElementToBeginningOfArray(array ,){
   
 }
